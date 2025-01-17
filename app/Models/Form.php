2b92b32bf20653edad934610b403e4f98a682c03
Ace <akhmadfauzy40@gmail.com>
2025-01-17 11:33:40 +0700

    Ketinggalan euy

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -40,7 +40,57 @@ class Form extends Model
 
     public function profile_resiko()
     {
-        return $this->hasMany(profile_resiko::class, 'form_id', 'id');
+        return $this->hasMany(ProfileRisiko::class, 'form_id', 'id');
+    }
+
+    public function issue_risiko()
+    {
+        return $this->hasMany(IssueRisiko::class, 'form_id', 'id');
+    }
+
+    public function evaluasi_manajemen_risiko()
+    {
+        return $this->hasMany(EvaluasiManajemenRisiko::class, 'form_id', 'id');
+    }
+
+    public function arahan_manajemen_risiko()
+    {
+        return $this->hasMany(ArahanManajemenRisiko::class, 'form_id', 'id');
+    }
+
+    public function pemantauan_manajemen_risiko()
+    {
+        return $this->hasMany(PemantauanManajemenRisiko::class, 'form_id', 'id');
+    }
+
+    public function pengumpulan_manajemen_risiko()
+    {
+        return $this->hasMany(PengumpulanManajemenRisiko::class, 'form_id', 'id');
+    }
+
+    public function menganalisis_risiko()
+    {
+        return $this->hasMany(MenganalisisRisiko::class, 'form_id', 'id');
+    }
+
+    public function memelihara_profil_risiko()
+    {
+        return $this->hasMany(MemeliharaProfilRisiko::class, 'form_id', 'id');
+    }
+
+    public function mengartikulasikan_risiko()
+    {
+        return $this->hasMany(MengartikulasiRisiko::class, 'form_id', 'id');
+    }
+
+    public function menentukan_portofolio_mitigasi_risiko()
+    {
+        return $this->hasMany(MenentukanPortofolioMitigasiRisiko::class, 'form_id', 'id');
+    }
+
+    public function menanggapi_risiko()
+    {
+        return $this->hasMany(MenanggapiRisiko::class, 'form_id', 'id');
     }
 
     protected static function boot()
